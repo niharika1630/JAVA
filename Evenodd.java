7743ffabd78ff20095767023c2aaf65a9c943d54
niharika1630 <niharika16.r@gmail.com>
2025-11-12 19:46:04 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,13 @@
+public class Evenodd {
+    public static void main(String[] args) {
+        int num=7;
+        if(num%2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+
+    }
+    
+}
---
+++ Typecast.java
@@ -0,0 +1,9 @@
+public class Typecast {
+    public static void main(String[] args) {
+        double d=9.8;
+        int i=(int)d;
+        System.out.println("double: "+ d);
+        System.out.println("integer: "+ i);
+    }
+    
+}
